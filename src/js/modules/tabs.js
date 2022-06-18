5813501527eb2c79dc0525858f217a6d3a136a89
Dima Bart <3088742@mail.ru>
2022-06-18 13:06:17 +0300

    add click tabs

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,11 +1,41 @@
-const tabs = () => {
+const tabs = (headerSelector, tabSelector, contentSlector, activeClass) => {
+	const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSlector);
+
+	 function hideTabContent() {
+		content.forEach(item => {
+			item.style.display = "none"
+		})
+
+		tab.forEach(item => {
+			item.classList.remove(activeClass)
+		})
+	 }
+
+	 function showTabContent(i = 0) {
+		content[i].style.display = "block";
+		tab[i].classList.add(activeClass);
+	 }
+
+	 hideTabContent();
+	 showTabContent();
 
 
-
-
-
-
-	
+	 header.addEventListener('click', (e) => {
+		const target = e.target
+		if(target &&
+			(target.classList.contains(tabSelector.replace(/\./, '')) || 
+			target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+				
+				tab.forEach((item, i) => {
+					if (target == item || target.parentNode == item) {
+						hideTabContent()
+						showTabContent(i)
+					}
+				})
+		}
+	 })
 }
 
 
